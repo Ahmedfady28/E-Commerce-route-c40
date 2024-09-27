6ec97d71e160b9411b6fac5e4abd04db07b7f0b6
Ahmed Morgan <elmorg6666@gmail.com>
2024-09-27 18:40:06 +0300

    feat:Design

--- app/src/main/java/com/example/e_commerce_route_c40/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce_route_c40/ui/activities/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.e_commerce_route_c40.ui.activities
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.e_commerce_route_c40.R
-import com.example.e_commerce_route_c40.databinding.ActivityMainBinding
 import com.example.e_commerce_route_c40.base.BaseActivity
-import com.example.e_commerce_route_c40.base.BaseViewModel
+import com.example.e_commerce_route_c40.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +20,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         linkNavHostWithBottomNavigation()
+        binding.bottomNavigation.visibility = View.GONE
+
+        linkNavHostWithBottomNavigation()
+        handleKeyboardVisibility()
 
     }
 
@@ -27,5 +32,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         binding.bottomNavigation.setupWithNavController(navController)
     }
 
+    private fun handleKeyboardVisibility() {
+        // Listen for layout changes
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            binding.root.getWindowVisibleDisplayFrame(rect)
 
+            // Get screen height and compare with visible window height
+            val screenHeight = binding.root.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // If more than 15% of the screen height is covered, the keyboard is open
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                // Keyboard is closed
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+
+        }
+    }
 }
